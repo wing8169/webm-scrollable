85eeeecfff3dc359a1ddf07ad71e1e47fdd264b7
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2024-04-12 14:26:20 +0800

    Add preload

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,36 @@ const fromFrame = 10000;
 const toFrame = 11379;
 const frameCount = toFrame - fromFrame;
 
+const images: string[] = [];
+for (let i = fromFrame; i <= toFrame; i++) {
+  images.push(`/sequence/${i.toString().padStart(8, "0")}.webp`);
+}
+
 function App() {
   const [frame, setFrame] = useState(fromFrame);
+  const [loading, setLoading] = useState(true);
+
+  const cacheImages = async () => {
+    const promises = await images.map((src) => {
+      return new Promise<void>((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          resolve();
+        };
+        img.onerror = () => {
+          reject();
+        };
+      });
+    });
+
+    await Promise.all(promises);
+
+    setLoading(false);
+  };
 
   useEffect(() => {
+    cacheImages();
     // if (!videoRef) return;
     gsap.registerPlugin(ScrollTrigger);
     // ScrollTrigger.normalizeScroll(true);
@@ -43,10 +69,14 @@ function App() {
         id="gsap-container"
         className="flex flex-col items-center w-screen h-[10000px]"
       >
-        <img
-          src={`/sequence/${frame.toString().padStart(8, "0")}.webp`}
-          className="fixed top-0 left-0 w-screen h-screen object-cover"
-        />
+        {loading ? (
+          <p>Loading</p>
+        ) : (
+          <img
+            src={`/sequence/${frame.toString().padStart(8, "0")}.webp`}
+            className="fixed top-0 left-0 w-screen h-screen object-cover"
+          />
+        )}
       </div>
     </ReactLenis>
   );
